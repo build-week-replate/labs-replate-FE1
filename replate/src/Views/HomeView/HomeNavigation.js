ae34e5834f6dc4573380d74252c86cc48cd3cb9a
blubrandi <blubrandi0@gmail.com>
2019-04-19 21:06:15 -0600

    Completed styling of HomeView Navigation and Login Form.  Added Routing to Register from Login Form.

--- replate/src/Views/HomeView/HomeNavigation.js
+++ replate/src/Views/HomeView/HomeNavigation.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import { NavLink }from 'react-router-dom'
 
+import styled from 'styled-components'
+
+const HomeNav = styled.div`
+    background-color: #006837;
+    height: 60px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    a {
+        color: #fff;
+        padding-right: 20px;
+        text-decoration: none;
+    }
+`
+
 const HomeNavigation = props => {
     return (
-        <div>
+        <HomeNav>
             <NavLink to='/login'>Login</NavLink>
             <NavLink to='/register'>Register</NavLink>
-        </div>
+        </HomeNav>
     )
 }
 
--- replate/src/Views/HomeView/Login.js
+++ replate/src/Views/HomeView/Login.js
@@ -1,8 +1,69 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import styled from 'styled-components'
 
 import { login } from '../../actions/login'
+
+const LoginForm = styled.div`
+    margin: 0 auto;
+    max-width: 600px;
+    border: 2px solid #8bc53e;
+    padding: 20px;
+    border-radius: 5px;
+    margin-top: 60px;
+    
+    form {
+        display: flex;
+        flex-direction: column;
+        margin: 0 auto;
+        margin-top: 40px;
+        max-width: 80%;
+    }
+
+    input {
+        padding: 15px;
+        margin-top: 5px;
+        margin-bottom: 15px;
+        border-radius: 3px;
+        border: 2px solid #8bc53e;
+    }
+
+    button {
+        background-color: #8bc53e;
+        color: #fff;
+        width: 125px;
+        margin: 0 auto;
+        margin-top: 25px;
+        margin-bottom: 25px;
+        border: 2px solid #8bc53e;
+        border-radius: 3px;
+        padding: 15px 20px;
+        font-size: 14px;
+        cursor: pointer;
+
+        :hover {
+            background-color: #006837;
+            border: 2px solid #006837;
+            color: #fff;
+        }
+    }
+
+    p {
+        color: #888;
+    }
+
+    h2 {
+        color: #006837;
+        font-size: 30px;
+    }
+
+    a {
+        color: #8bc53e;
+        text-decoration: none;
+    }
+
+`
 
 class Login extends React.Component {
     state = {
@@ -32,19 +93,23 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
+            <LoginForm>
+                <h2>Welcome Back!</h2>
+                <p>Please Login to Continue</p>
                 <form onSubmit={this.login}>
                     <input
                         type='text'
                         name='email'
                         value={this.state.credentials.username}
                         onChange={this.handleChanges}
+                        placeholder='email'
                     />
                     <input
                         type='password'
                         name='password'
                         value={this.state.credentials.password}
                         onChange={this.handleChanges}
+                        placeholder='password'
                     />
                     <button>
                         {this.props.loggingIn ? (
@@ -53,9 +118,12 @@ class Login extends React.Component {
                             'Log In'
                         )}
                     </button>
+                    <p>
+                        Not a member?  <Link to='/register'>Please Register!</Link>
+                    </p>
                 </form>
                 {this.props.error && <p className="error">{this.props.error}</p>}
-            </div>
+            </LoginForm>
         )
     }
 }
